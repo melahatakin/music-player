3e5fb8f6e4fabf182a07e89b1db27f98cae9a5f3
melahatakin <56938288+melahatakin@users.noreply.github.com>
2020-05-25 15:49:44 +0300

    init
    Initialization project files

--- app/src/main/java/com/example/mymusic/MainActivity.java
+++ app/src/main/java/com/example/mymusic/MainActivity.java
@@ -1,4 +1,5 @@
-package com.example.musicplayer;
+
+package com.example.mymusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.customview.widget.ViewDragHelper;
@@ -7,6 +8,7 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
@@ -34,11 +37,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-           }
-           private static final String[] PERMISSIONS = {
+    }
+    private static final String[] PERMISSIONS = {
             Manifest.permission.READ_EXTERNAL_STORAGE
 
-           };
+    };
     private static final int REQUEST_PERMISSIONS = 12345;
     private static final int PERMISSIONS_COUNT = 1;
     @SuppressLint("NewApi")
@@ -48,15 +51,16 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         }
-         return false;
+        return false;
     }
+
     @SuppressLint("NewApi")
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
         super.onRequestPermissionsResult(requestCode ,permissions,grantResults);
         if(arePermissionsDenied()){
             ((ActivityManager) (this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
-recreate();
+            recreate();
         }else{
             onResume();
         }
@@ -77,13 +81,25 @@ recreate();
             }
         }
     }
-    private void fillMusicList(){
+    private void fillMusicList() {
         musicFilesList.clear();
         addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_MUSIC)));
         addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)));
-
     }
+        private void playMusicFile(String path){
+            MediaPlayer mp= new MediaPlayer();
+            try{
+                mp.setDataSource(path);
+                mp.prepare();
+                mp.start();
+
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
     @Override
     protected void onResume(){
         super.onResume();
@@ -98,49 +114,54 @@ recreate();
             fillMusicList();
             textAdapter.setData(musicFilesList);
             listView.setAdapter(textAdapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    final String musicFilePath=musicFilesList.get(position);
+                }
+            });
             isMusizPlayerInit= true;
         }
     }
-    class TextAdapter extends BaseAdapter{
-        private List<String> data =new ArrayList<>();
-        void setData(List<String> mData){
-            data.clear();
-            data.addAll(mData);
-            notifyDataSetChanged();
-        }
-        @Override
-        public int getCount(){
-            return data.size();
-        }
-        @Override
-        public String getItem(int position){
-            return null;
-        }
-        @Override
-        public long getItemId(int position){
-            return 0;
-        }
-        @Override
-        public View getView(int position, View converView, ViewGroup parent) {
-            if (converView == null) {
-                converView= LayoutInflater.from(parent.getContext()).
-                        inflate(R.layout.item, parent, false);
-                converView.setTag(new ViewHolder((TextView) converView.findViewById(R.id.myItem)));
-
-            }
-            ViewHolder holder= (ViewHolder) converView.getTag();
-            final String item= data.get(position);
-            holder.info.setText(item.substring( item.lastIndexOf('/')+1));
-            return converView;
+class TextAdapter extends BaseAdapter{
+    private List<String> data =new ArrayList<>();
+    void setData(List<String> mData){
+        data.clear();
+        data.addAll(mData);
+        notifyDataSetChanged();
+    }
+    @Override
+    public int getCount(){
+        return data.size();
+    }
+    @Override
+    public String getItem(int position){
+        return null;
+    }
+    @Override
+    public long getItemId(int position){
+        return 0;
+    }
+    @Override
+    public View getView(int position, View converView, ViewGroup parent) {
+        if (converView == null) {
+            converView= LayoutInflater.from(parent.getContext()).
+                    inflate(R.layout.item, parent, false);
+            converView.setTag(new ViewHolder((TextView) converView.findViewById(R.id.myItem)));
 
         }
-        class ViewHolder{
-            TextView info;
-            ViewHolder(TextView mInfo){
-                info= mInfo;
-
-            }
-        }
+        ViewHolder holder= (ViewHolder) converView.getTag();
+        final String item= data.get(position);
+        holder.info.setText(item.substring( item.lastIndexOf('/')+1));
+        return converView;
 
     }
-}
+    class ViewHolder{
+        TextView info;
+        ViewHolder(TextView mInfo){
+            info= mInfo;
+
+        }
+    }
+
+}}
